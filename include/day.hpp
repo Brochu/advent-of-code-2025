8821b4651db0a30a05b60c0752f48d4a6f0ac15b
Alex Brochu <alexbrochu1@gmail.com>
2025-12-01 22:07:31 -0800

    [DEV] Day2 - Part1 completed

--- include/day.hpp
+++ include/day.hpp
@@ -6,4 +6,17 @@
 #define SV_ARG(sv) (int)sv.len, sv.ptr
 #define print_res(out, fmt, ...) sprintf_s(out, ANS_SIZE, fmt, __VA_ARGS__)
 
+#include <cstdint>
+using i8  =   int8_t;
+using u8  =  uint8_t;
+using i16 =  int16_t;
+using u16 = uint16_t;
+using i32 =  int32_t;
+using u32 = uint32_t;
+using i64 =  int64_t;
+using u64 = uint64_t;
+
+using f32 = float;
+using f64 = double;
+
 int solve(char p1[ANS_SIZE], char p2[ANS_SIZE]);
--- src/day02.cpp
+++ src/day02.cpp
@@ -2,27 +2,70 @@
 
 #include "day.hpp"
 #include "parse.hpp"
-#include "simd.hpp"
+//#include "simd.hpp"
 
+#if 1
 char in[] = {
     #include "../inputs/day02.inc"
 };
-strview input { in, strlen(in) };
-
-char t[] = {
-    ""
+#else
+char in[] = {
+    "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124"
 };
-strview test { t, strlen(t) };
+#endif
+strview input { in, strlen(in) };
 
 static const char *ispc_src = "./shaders/day02.ispc";
 
 //static const char *ispc_fn = "sums";
 //using sums_func_t = void (*)(int vals[], int starts[], int lens[], int outs[], int count);
 
-int solve(char p1[ANS_SIZE], char p2[ANS_SIZE]) {
-    auto engine = compile_ispc({ "", "" /*"--target=host", "-O2"*/ }, ispc_src);
+bool validate_num(i64 num) {
+    u64 size = 0;
+    u64 lo = 1;
+    while (lo < num) {
+        lo *= 10;
+        size++;
+    }
+    //printf("[CHECK] num = %lld ; size = %lld\n", num, size);
 
-    print_res(p1, "%i", 0);
-    print_res(p2, "%i", 0);
+    if ((size % 2) != 0) {
+        return false; // Odd sized nums cannot be invalid
+    }
+
+    lo = 0;
+    for (int i = 0; i < size/2; i++) {
+        i64 rem = num % 10;
+        rem *= pow(10, i);
+        lo += rem;
+        num /= 10;
+    }
+    //printf("[CHECK] hi = %lld ; lo = %lld\n", num, lo);
+    return num == lo;
+}
+
+
+int solve(char p1[ANS_SIZE], char p2[ANS_SIZE]) {
+    //auto engine = compile_ispc({ "", "" /*"--target=host", "-O2"*/ }, ispc_src);
+
+    u64 sum_p1 = 0;
+    for (strview &elem : sv_split(input, ",")) {
+        strview min_str, max_str;
+        sv_split_once(elem, "-", &min_str, &max_str);
+
+        i64 min, max;
+        min = atoll(min_str.ptr);
+        max = atoll(max_str.ptr);
+
+        for (i64 num = min; num <= max; num++) {
+            if (validate_num(num)) {
+                //printf("[VALID] num = %lld\n", num);
+                sum_p1 += num;
+            }
+        }
+    }
+
+    print_res(p1, "%lld", sum_p1);
+    print_res(p2, "%lld", 0LL);
     return 0;
 }
