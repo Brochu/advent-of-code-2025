44a9877a877e1bab2cba46927db6ac034173138f
Alex Brochu <alexbrochu1@gmail.com>
2025-12-05 23:37:15 -0800

    [DEV] Day5 - Part2 some progress, but need a different approach

--- src/day05.cpp
+++ src/day05.cpp
@@ -27,12 +27,16 @@ strview input { in, strlen(in) };
 
 static const char *ispc_src = "./shaders/day05.ispc";
 
-static const char *ispc_fn = "solve";
-using solve_func_t = i64 (*)(i64 ls[], i64 hs[], i64 num_ranges, i64 ids[], i64 num_ids);
+static const char *part1_fn_name = "part1";
+using part1_fn_t = i64 (*)(i64 ls[], i64 hs[], i64 num_ranges, i64 ids[], i64 num_ids);
+
+static const char *part2_fn_name = "part2";
+using part2_fn_t = i64 (*)(i64 ls[], i64 hs[], i64 num_ranges);
 
 int solve(char p1[ANS_SIZE], char p2[ANS_SIZE]) {
-    auto engine = compile_ispc({ "", "" /*"--target=host", "-O2"*/ }, ispc_src);
-    solve_func_t solve_fn = (solve_func_t)engine->GetJitFunction(ispc_fn);
+    auto engine = compile_ispc({ "-g" }, ispc_src);
+    part1_fn_t part1_fn = (part1_fn_t)engine->GetJitFunction(part1_fn_name);
+    part2_fn_t part2_fn = (part2_fn_t)engine->GetJitFunction(part2_fn_name);
 
     strview r_str, i_str;
     sv_split_once(input, "\n\n", &r_str, &i_str);
@@ -51,16 +55,10 @@ int solve(char p1[ANS_SIZE], char p2[ANS_SIZE]) {
         ids.emplace_back(atoll(i.ptr));
     }
 
-    /*
-    for (u64 i = 0; i < ids.size(); i++) {
-        //printf("(%lld, %lld)\n", ls[i], hs[i]);
-        printf(" -> %lld\n", ids[i]);
-    }
-    */
+    i64 value_p1 = part1_fn(ls.data(), hs.data(), ls.size(), ids.data(), ids.size());
+    i64 value_p2 = part2_fn(ls.data(), hs.data(), ls.size());
 
-    i64 res = solve_fn(ls.data(), hs.data(), ls.size(), ids.data(), ids.size());
-
-    print_res(p1, "%lld", res);
-    print_res(p2, "%lld", 0LL);
+    print_res(p1, "%lld", value_p1);
+    print_res(p2, "%lld", value_p2);
     return 0;
 }
