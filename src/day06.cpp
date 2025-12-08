9655303e351f7f2e4977649eb9a4e7e7e01a66cb
Alex Brochu <alexbrochu1@gmail.com>
2025-12-07 23:18:08 -0800

    [DEV] Day6 - Part2 completed with new style of parsing, yark

--- src/day06.cpp
+++ src/day06.cpp
@@ -4,7 +4,7 @@
 #include "parse.hpp"
 #include "simd.hpp"
 
-#if 0
+#if 1
 char in[] = {
     #include "../inputs/day06.inc"
 };
@@ -43,8 +43,8 @@ int solve(char p1[ANS_SIZE], char p2[ANS_SIZE]) {
     std::vector<i64> nums_p1;
 
     i64 base = 0;
-    i64 end = 1;
-    while(end < op_line.len) {
+    i64 end;
+    for (end = 1; end < op_line.len; end++) {
         if (op_line.ptr[end] != ' ') {
             for (i32 i = 0; i < lines.size()-1; i++) {
                 nums_p1.emplace_back(parse_num(lines[i], base, end));
@@ -52,8 +52,6 @@ int solve(char p1[ANS_SIZE], char p2[ANS_SIZE]) {
             ops_p1.emplace_back(op_line.ptr[base] - '*');
             base = end;
         }
-
-        end++;
     }
 
     for (i32 i = 0; i < lines.size()-1; i++) {
@@ -61,8 +59,31 @@ int solve(char p1[ANS_SIZE], char p2[ANS_SIZE]) {
     }
     ops_p1.emplace_back(op_line.ptr[base] - '*');
 
+    std::vector<i8> ops_p2;
+    std::vector<i64> nums_p2;
+    base = op_line.len;
+    for (end = base - 1; end >= 0; end--) {
+        if (op_line.ptr[end] != ' ') {
+            for (i32 j = 0; j < (base-end); j++) {
+                i64 val = 0;
+                for (i32 i = 0; i < lines.size()-1; i++) {
+                    char c = lines[i].ptr[base-1-j];
+                    if (c != ' ') {
+                        val = (val*10) + (c - '0');
+                    }
+                }
+                nums_p2.emplace_back(val);
+            }
+            ops_p2.emplace_back(op_line.ptr[end] - '*');
+            for (i32 i = 0; i < 4 - (base-end); i++) {
+                nums_p2.emplace_back(0); // PADDING
+            }
+            base = end - 1;
+        }
+    }
+
     i64 total_p1 = solve_fn(nums_p1.data(), ops_p1.data(), ops_p1.size(), lines.size()-1);
-    i64 total_p2 = 0;
+    i64 total_p2 = solve_fn(nums_p2.data(), ops_p2.data(), ops_p2.size(), 4);
 
     print_res(p1, "%lld", total_p1);
     print_res(p2, "%lld", total_p2);
