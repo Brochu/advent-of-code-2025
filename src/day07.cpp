dc44fd9dded3575b2099d4ad9e960a2ddfafc5c2
Alex Brochu <alexbrochu1@gmail.com>
2025-12-08 09:16:18 -0800

    [DEV] Day7 - sharing coords between c++ and ISPC

--- src/day07.cpp
+++ src/day07.cpp
@@ -35,6 +35,13 @@ static const char *ispc_src = "./shaders/day07.ispc";
 static const char *step_fn_name = "step";
 using step_fn_t = void (*)(u64 beams[], u64 num_beams, u64 *num_splits);
 
+struct coord {
+    union {
+        struct { i32 x; i32 y; };
+        u64 key;
+    };
+};
+
 int solve(char p1[ANS_SIZE], char p2[ANS_SIZE]) {
     auto engine = compile_ispc({ "" }, ispc_src);
     step_fn_t step_fn = (step_fn_t)engine->GetJitFunction(step_fn_name);
@@ -44,8 +51,9 @@ int solve(char p1[ANS_SIZE], char p2[ANS_SIZE]) {
     step_fn(nullptr, 69, &num_splits);
     printf(" -> num_splits = %lld\n", num_splits);
 
-    step_fn(nullptr, 420, &num_splits);
-    printf(" -> num_splits = %lld\n", num_splits);
+    coord test;
+    test.key = num_splits;
+    printf("Got coord from ISPC : (%i, %i)\n", test.x, test.y);
 
     print_res(p1, "%lld", 0LL);
     print_res(p2, "%lld", 0LL);
