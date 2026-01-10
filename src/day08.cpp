318ba7017664e05b903e1f5ea02036ea5f2a8790
Alex Brochu <alexbrochu1@gmail.com>
2026-01-10 00:44:31 -0800

    [DEV] Day8 - Part2 some progress and rewrite, still some quirks with ISPC

--- src/day08.cpp
+++ src/day08.cpp
@@ -9,7 +9,7 @@
 char in[] = {
     #include "../inputs/day08.inc"
 };
-const static i32 ITER = 1000;
+const static i32 num_connection = 1000;
 #else
 char in[] = {
 "162,817,812\n"
@@ -33,29 +33,73 @@ char in[] = {
 "984,92,344\n"
 "425,690,689\n"
 };
-const static i32 ITER = 10;
+const static i32 num_connection = 10;
 #endif
 strview input { in, strlen(in) };
 
 struct boxes {
-    std::vector<i64> xs;
-    std::vector<i64> ys;
-    std::vector<i64> zs;
+    std::vector<i32> xs;
+    std::vector<i32> ys;
+    std::vector<i32> zs;
+
+    std::vector<i32> c_ids;
+    std::vector<i32> c_sizes;
 };
+
+struct combos {
+    std::vector<i32> from;
+    std::vector<i32> to;
+    std::vector<i64> dist;
+};
+
+i32 pivot(combos &c, i32 lo, i32 hi) {
+    i64 pivot = c.dist[hi];
+    i32 i = lo;
+
+    for (i32 j = lo; j < hi; j++) {
+        if (c.dist[j] <= pivot) {
+            std::swap(c.dist[j], c.dist[i]);
+            std::swap(c.from[j], c.from[i]);
+            std::swap(c.to[j], c.to[i]);
+            i++;
+        }
+    }
+
+    std::swap(c.dist[i], c.dist[hi]);
+    std::swap(c.from[i], c.from[hi]);
+    std::swap(c.to[i], c.to[hi]);
+    return i;
+}
+
+void quicksort(combos &c, i32 lo, i32 hi) {
+    if (lo >= hi || lo < 0) {
+        return;
+    }
+
+    i32 p = pivot(c, lo, hi);
+
+    quicksort(c, lo, p-1);
+    quicksort(c, p+1, hi);
+}
 
 static const char *ispc_src = "./shaders/day08.ispc";
 
-static const char *solve_fn_name = "solvep1";
-using solve_fn_t = i64 (*)(i64 x[], i64 y[], i64 z[], i64 num_boxes, i64 num_iter, i64 from[], i64 to[], i64 dists[], i32 ids[], i32 sizes[]);
+static const char *dist_fn_name = "dist";
+using dist_fn_t = void (*)(i32 x[], i32 y[], i32 z[], i32 num_boxes, i32 from[], i32 to[], i64 dists[]);
+
+static const char *cnnx_fn_name = "connect";
+using cnnx_fn_t = i64 (*)(i32 from[], i32 to[], i32 num_connect, i32 c_ids[], i32 c_sizes[], i32 num_boxes);
 
 int solve(char p1[ANS_SIZE], char p2[ANS_SIZE]) {
     auto engine = compile_ispc({ "-g" }, ispc_src);
-    solve_fn_t solvep1_fn = (solve_fn_t)engine->GetJitFunction(solve_fn_name);
+    dist_fn_t dist_fn = (dist_fn_t)engine->GetJitFunction(dist_fn_name);
+    cnnx_fn_t cnnx_fn = (cnnx_fn_t)engine->GetJitFunction(cnnx_fn_name);
 
     boxes b;
-    for(strview box_line : sv_split(input, "\n")) {
+    std::vector<strview> box_lines = sv_split(input, "\n");
+    for(i32 i = 0; i < box_lines.size(); i++) {
         strview lo, rest;
-        sv_split_once(box_line, ",", &lo, &rest);
+        sv_split_once(box_lines[i], ",", &lo, &rest);
 
         strview mid, hi;
         sv_split_once(rest, ",", &mid, &hi);
@@ -63,65 +107,36 @@ int solve(char p1[ANS_SIZE], char p2[ANS_SIZE]) {
         b.xs.emplace_back(atoi(lo.ptr));
         b.ys.emplace_back(atoi(mid.ptr));
         b.zs.emplace_back(atoi(hi.ptr));
+
+        b.c_ids.emplace_back(i);
+        b.c_sizes.emplace_back(1);
     }
     const size_t n = b.xs.size();
 
-    printf("[DEBUG] Found %lld boxes\n", b.xs.size());
-    std::vector<i64> f_idx, t_idx, dists;
-    f_idx.resize((n*(n-1))/2, -1);
-    t_idx.resize((n*(n-1))/2, -1);
-    dists.resize((n*(n-1))/2, -1);
+    printf("[DEBUG] Found %lld boxes\n", n);
+    combos c;
+    c.from.resize(n*(n-1)/2, -1);
+    c.to.resize(n*(n-1)/2, -1);
+    c.dist.resize(n*(n-1)/2, 0);
 
-    std::vector<i32> circ_ids, circ_sizes;
-    circ_ids.resize(n);
-    circ_sizes.resize(n, 1);
-    for (i32 i = 0; i < n; i++) {
-        circ_ids[i] = i;
-    }
-
-    i64 unsued = solvep1_fn(
-        b.xs.data(), b.ys.data(), b.zs.data(), b.xs.size(),
-        ITER,
-        f_idx.data(), t_idx.data(), dists.data(),
-        circ_ids.data(), circ_sizes.data());
+    dist_fn(b.xs.data(), b.ys.data(), b.zs.data(), n, c.from.data(), c.to.data(), c.dist.data());
+    quicksort(c, 0, c.dist.size());
 
     /*
-    for (i32 i = 0; i < f_idx.size(); i++) {
-        i64 f = f_idx[i];
-        i64 t = t_idx[i];
-        printf(" - [%lld] From: %lld (%lld, %lld, %lld) -> To: %lld (%lld, %lld, %lld)\n",
-           dists[i],
-           f, b.xs[f], b.ys[f], b.zs[f],
-           t, b.xs[t], b.ys[t], b.zs[t]);
-    }
-
-    for (i32 i = 0; i < n; i++) {
-        i32 id = circ_ids[i];
-        i32 size = circ_sizes[id];
-
-        printf(" - Box #%i : id = %i (size = %i)\n", i, id, size);
+    for (i32 i = 0; i < (n*(n-1)/2); i++) {
+        printf(" - (%i) -- (%i) / {%lli}\n", c.from[i], c.to[i], c.dist[i]);
     }
     */
 
-    i64 last = -1;
-    i32 count = 0;
-    i64 p1_res = 1;
-    std::make_heap(circ_sizes.begin(), circ_sizes.end());
+    i64 res_p1 = cnnx_fn(c.from.data(), c.to.data(), num_connection, b.c_ids.data(), b.c_sizes.data(), n);
 
-    while (circ_sizes.size() > 0 && count < 3) {
-        std::pop_heap(circ_sizes.begin(), circ_sizes.end());
-        i32 next = circ_sizes.back();
-        circ_sizes.pop_back();
-        printf(" - size = %i\n", next);
-
-        if (next != last) {
-            p1_res *= next;
-            last = next;
-            count++;
-        }
+    for (i32 i = 0; i < n; i++) {
+        printf(" - [(%i, %i, %i)] : ID = %i; SIZE = %i\n", b.xs[i], b.ys[i], b.zs[i], b.c_ids[i], b.c_sizes[i]);
     }
 
-    print_res(p1, "%lld", p1_res);
-    print_res(p2, "%i", 0);
+    //i64 res_p1 = 0;
+    i64 res_p2 = 0;
+    print_res(p1, "%lli", res_p1);
+    print_res(p2, "%lli", res_p2);
     return 0;
 }
